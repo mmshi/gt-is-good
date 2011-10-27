a0a5f863aa6c8887e3003f452b7b39887cd1add1
desposi1 <desposi1@gmail.com>
2011-10-27 02:39:25 +0000

    Fixed SQL code. Working for get... Insert/Create needs to be implemented.

--- src0.1/scripts/GtIsGoodServices.php
+++ src0.1/scripts/GtIsGoodServices.php
@@ -7,42 +7,87 @@ include_once "GtIsGoodObjects.php";
  */
 function getScheduleById($id) {
 	// TODO
-	return new GTIGSchedule($id, 1, date_create(date('D, d M Y H:i:s')), date_create(date('D, d M Y H:i:s')), "Sample Schedule: $id", GTIGScheduleTypes::HALFHOUR);
+	$sql="SELECT * FROM `gtisgood`.`schedule` WHERE `schID`=$id";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	if($rowcount<1)return null;
+	return parseScheduleRow(mysql_fetch_array($result));
 }
 
-function getSchedulesByAlias($alias) {
+function getScheduleByAlias($alias) {
 	// TODO
-	return new GTIGSchedule(1, 1, date_create(date('D, d M Y H:i:s')), date_create(date('D, d M Y H:i:s')), $alias, GTIGScheduleTypes::HALFHOUR);
+	$sql="SELECT * FROM `gtisgood`.`schedule` WHERE `alias`='$alias';";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	if($rowcount<1)return null;
+	return parseScheduleRow(mysql_fetch_array($result));
 }
 
 function getAllSchedules() {
-	// TODO
-	$arr = array();
-	for ($i=0;$i<10;$i++)
-		$arr[] = getScheduleById($id);
-	return $arr;
+        // TODO
+	$sql="SELECT * FROM `gtisgood`.`schedule`;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	$i=0;
+	$arr= array();
+	while($row = mysql_fetch_array($result))
+		$arr[] = parseScheduleRow($row);
+    return $arr;
 }
 
 function getAllSchedulesByCreator($creatorId) {
-	// TODO
-	return getAllSchedules();
+	$sql="SELECT * FROM `gtisgood`.`schedule` WHERE `creatorID`=$creatorId;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	$i = 0;
+	$arr = array();
+	while($row = mysql_fetch_array($result))
+		$arr[] = parseScheduleRow($row);
+	return $arr;
+}
+
+function  parseScheduleRow($row) {
+	$creatorId = $row["creatorID"];
+	$startDate= $row["startDate"];
+	$endDate= $row["endDate"];
+	$id= $row["schID"];
+	$type= $row["periodType"];
+	return new GTIGSchedule($id, $createrId, $startDate, $endDate, $alias, $type);
 }
 
 
 /**
  * GTIGGrid Services
  */
-function getGridById($id) {
+function getGridById($gid, $sid) {
 	// TODO
-	return new GTIGGrid($id, 1, GTIGGridTypes::SCHEDULE, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", ""); // 8-11 x 4
+	$sql="SELECT * FROM `gtisgood`.`grid`, `gtisgood`.`linktable` WHERE `grid`.`gridID`=$gid AND `linktable`.`gridID`=$gid AND `linktable`.`schID`=$sid;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	if($rowcount<1)return null;
+	return parseGridRow(mysql_fetch_array($result));
 }
 
 function getGridsBySchedule($schId) {
 	// TODO
+	$sql="SELECT * FROM `gtisgood`.`grid`, `gtisgood`.`linktable` WHERE `grid`.`gridID`=`linktable`.`gridID` AND `linktable`.`schID`=$sid;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	$i=0;
 	$arr = array();
-	for ($i=0;$i<10;$i++)
-		$arr[] = getGridById($id);
+	while($row = mysql_fetch_array($result))
+		$arr[] = parseGridRow($row);
 	return $arr;
+}
+
+// PRIVATE
+function parseGridRow($row) {
+	$id= $row["gridID"];
+	$userId= $row["userID"];
+	$type= $row["type"];
+	$data= $row["data"];
+	$comments= $row["comments"];
+	return new GTIGGrid($id, $userId, $type, $data, $comments);
 }
 
 
@@ -51,20 +96,42 @@ function getGridsBySchedule($schId) {
  */
 function getUserById($id) {
 	// TODO
-	return new GTIGUser($id, "David", "", "", 0);
+	$sql="SELECT * FROM `gtisgood`.`user` WHERE `userID`=$id;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	if($rowcount<1)return null;
+	return parseUserRow(mysql_fetch_array($result));
 }
 
 function getUserByEmail($email) {
 	// TODO
-	return getUserById(1);
+	$sql="SELECT * FROM `gtisgood`.`user` WHERE `email`='$email';";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	if($rowcount<1)return null;
+	return parseUserRow(mysql_fetch_array($result));
 }
 
 function getAllUsers() {
 	// TODO
-	$arr = array();
-	for ($i=0;$i<10;$i++)
-		$arr[] = getUserById($id);
-	return $arr;
+	$sql="SELECT * FROM `gtisgood`.`user`;";
+	$result = mysql_query($sql);
+	$rowcount = mysql_numrows($result);
+	$i=0;
+	$arr= array($rowcount);
+	while($row = mysql_fetch_array($result))
+		$arr[] = parseUserRow($row);
+    return $arr;
+}
+
+// PRIVATE
+function parseUserRow($row) {
+	$id = $row["userID"];
+	$name = $row["name"];
+	$email = $row["email"];
+	$password = $row["password"];
+	$fromTsquare = $row["fromTsquare"];
+	return new GTIGUser($id, $name,$email,$password,$fromTsquare);
 }
 
 ?>
